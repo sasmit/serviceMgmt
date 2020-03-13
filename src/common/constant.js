e0b0b68e8715cf88ce6a3cd6290a5d02226ee66c
pathansalman805 <38125145+pathansalman805@users.noreply.github.com>
2020-03-13 15:48:07 +0530

    Update constant.js
    all api urls

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,3 +1,34 @@
 const SERVER_ADDRESS = {
-    LOCAL: "http://10.12.137.16"
-} 
+    LOCAL_URL: "http://10.12.137.16",
+    API: {
+        USER_LOGIN: "/user/login",
+        USER_REGISTRATION :"/user/save",
+        USER_DETAILS :"/user",
+        user_by_id:"/user/:id", // ..../user/1
+        user_delete:"/user/delete",
+        user_update :"/user/update",
+
+        vendor_login :"/vendoe/login",
+        vendor_registration :"/vendor/save",
+        vendor_details:"/vendor",
+        vendor_by_id:"/vendor/:id",
+        vendor_update :"/vendor/update",
+        vendor_delete:"/vendoe/delete",
+
+        owner_login:"/owner/login",
+        owner_registration:"/owner/save",
+        owner_details:"/owner",
+        owner_by_id :"/owner/:id",
+        owner_update:"/owner/update",
+        owner_delete:"/owner/del/:id",
+
+
+        issue_save:"/issue/save",
+        issue_vendorname_update:"/issue/VNupdate",
+        get_all_issue:"/issue",
+        get_issue_by_status:"/issue/as/:st",
+        get_issue_by_trkingid:"/issue/:tid"
+    }
+}
+
+export default SERVER_ADDRESS;
